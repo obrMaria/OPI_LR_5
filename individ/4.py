58fb78b8ec1e949af4f6b003cd1616a4d64ca634
obrMaria <midsummer.madness@mail.ru>
2022-11-29 01:08:20 +0300

    ind pov

---
+++ individ/4.py
@@ -0,0 +1,24 @@
+#!\usr\bin\env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import math
+
+# Постоянная Эйлера.
+EULER = 0.5772156649015328606
+# Точность вычислений.
+EPS = 1e-10
+
+if __name__ == '__main__':
+    x = float(input("Value of x? "))
+    if x == 0:
+        print("Illegal value of x", file=sys.stderr)
+        exit(1)
+    a = x
+    S, n = a, 1
+    while math.fabs(a) > EPS:
+        a *= ((x**2) * (2*n + 1)) / (((2*n + 3)**2) * (2*n + 2))
+        S += a
+        n += 1
+
+print(f"Es({x}) = {EULER + math.log(math.fabs(x)) + S}")
